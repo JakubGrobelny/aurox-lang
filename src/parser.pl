f55341b627bf14dc929fe250c5c7b8c710d166d1
Jakub Grobelny <kubissus@gmail.com>
2019-04-21 19:35:07 +0200

    Dynamic operator declarations

--- src/parser.pl
+++ src/parser.pl
@@ -1,12 +1,87 @@
 :- [lexer].
-:- op(1200,xfx,==>).
+% :- op(1200,xfx,==>).
 
 parse_file(FileName, AST) :-
     tokenize_file(FileName, Tokens),
+    empty_operator_list(Ops),
     catch(
-        phrase(parser(AST), Tokens), 
+        phrase(parser(AST, Ops), Tokens), 
         error(Format, Args) at Pos, 
         print_error(Pos, FileName, Format, Args)
     ).
 
-parser([]) --> [].
+parser(Tokens, Operators) -->
+    [keyword(defop) at Pos],
+    !,
+    operator_definition(Pos, Operators, NewOperators),
+    parser(Tokens, NewOperators).
+parser([], _) --> [].
+
+operator_definition(_, PrevOperators, NewOperators) -->
+    ['{' at _],
+    [operator(Op) at _],
+    [integer(Priority) at PriorityPos],
+    [id(Associativity) at AssocPos],
+    ['}' at _],
+    {
+        valid_priority(Priority, PriorityPos),
+        valid_associativity(Associativity, AssocPos),
+        update_operators(
+            Op, 
+            Priority, 
+            Associativity, 
+            PrevOperators, 
+            NewOperators
+        )
+    },
+    !.
+
+valid_priority(integer(N), _) :-
+    member(N, [0,1,2,3,4,5]),
+    !.
+valid_priority(integer(N), Pos) :-
+    throw(
+        error('Invalid operator prority ~w in operator declaration', [N]) at Pos
+    ).
+
+valid_associativity(id(Assoc), _) :-
+    member(Assoc, [left, right, none, left_unary, right_unary]),
+    !.
+valid_associativity(id(Assoc), Pos) :-
+    throw(
+        error(
+            'Invalid operator associativity type ~w in operator declaration. \c
+             Allowed types are left, right, none, left_unary and right_unary', 
+            [Assoc]
+        ) at Pos
+    ).
+
+empty_operator_list(
+        ops([[[], [], [], [], [], []],
+             [[], [], [], [], [], []],
+             [[], [], [], [], [], []],
+             [[], [], [], [], [], []],
+             [[], [], [], [], [], []]])
+).
+
+translate_assoc(left, 0) :- 
+    !.
+translate_assoc(right, 1) :- 
+    !.
+translate_assoc(none, 2) :-
+    !.
+translate_assoc(left_unary, 3) :- 
+    !.
+translate_assoc(right_unary, 4).
+
+update_operators(Op, Priority, Assoc, ops(Ops), ops(NewOps)) :-
+    translate_assoc(Assoc, AssocNum),
+    get_nth(Ops, AssocNum, OpsWithAssoc, RestAssocs),
+    get_nth(OpsWithAssoc, Priority, OpsWithPriority, RestPriority),
+    select(Op, NewOpsWithPriority, OpsWithPriority),
+    put_nth(RestPriority, Priority, NewOpsWithPriority, NewOpsWithAssoc),
+    put_nth(RestAssocs, AssocNum, NewOpsWithAssoc, NewOps).
+
+
+
+
--- src/utility.pl
+++ src/utility.pl
@@ -5,3 +5,22 @@ print_error(pos(L, C), FileName, MessageFormat, MessageArgs) :-
     ),
     format(FinalFormat, [FileName, L, C | MessageArgs]),
     halt.
+
+get_nth([X | Xs], 0, X, Xs) :-
+    !.
+get_nth([X | Xs], N, Y, [X | Ys]) :-
+    !,
+    M is N - 1,
+    get_nth(Xs, M, Y, Ys).
+get_nth([], _, _, _) :-
+    throw(out_of_bounds_error).
+
+put_nth(Xs, 0, X, [X | Xs]) :-
+    !.
+put_nth([X | Xs], N, Elem, [X | Ys]) :-
+    !,
+    M is N - 1,
+    put_nth(Xs, M, Elem, Ys).
+put_nth([], _, _, _) :-
+    throw(out_of_bounds_error).
+
