e62bb147e673feded659dde75fd34fcd7d76bf3f
JakubGrobelny <300481@uwr.edu.pl>
2019-05-03 00:39:55 +0200

    Minor fixes

--- src/parser_rewrite.pl
+++ src/parser_rewrite.pl
@@ -1,10 +1,10 @@
 :- [lexer].
 
-parser_file(FileName, AST) :-
+parse_file(FileName, AST) :-
     tokenize_file(FileName, Tokens),
     empty_operator_list(Operators),
     catch(
-        phrase(program(AST, Ops), Tokens),
+        phrase(program(AST, Operators), Tokens),
         error(Format, Args) at Pos,
         print_error(Pos, Format, Args)
     ).
@@ -28,7 +28,7 @@ program([Import | Program], Operators) -->
 program([Definition | Program], Operators) -->
     [keyword(define) at DefineStart],
     !,
-    define_statement(DefineStart, Definition),
+    define_statement(DefineStart, Definition, Operators),
     program(Program, Operators).
 program([TypeDefinition | Program], Operators) -->
     [keyword(type) at TypeDefStart],
@@ -37,8 +37,9 @@ program([TypeDefinition | Program], Operators) -->
     program(Program, Operators).
 program([Expression | Program], Operators) -->
     peek(_ at ExprStart),
-    !
-    expression_top_level(ExprStart, Expression).
+    !,
+    expression_top_level(ExprStart, Expression, Operators),
+    program(Program, Operators).
 program([], _) --> [].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -53,8 +54,8 @@ import_statement(Start, import(Files)) -->
 
 list_of_files_to_import(Start, [File | Files]) -->
     file_name_to_import(Start, File),
-    !.
-    list_of_files_to_import(Start, Files),
+    !,
+    list_of_files_to_import(Start, Files).
 list_of_files_to_import(_, []) --> [].
 
 file_name_to_import(_, module_name(Module) at Pos) -->
@@ -63,7 +64,7 @@ file_name_to_import(_, module_name(Module) at Pos) -->
 file_name_to_import(_, file_name(File) at Pos) -->
     [string(File) at Pos],
     !.
-file_name_to_import(Start, _) -->
+file_name_to_import(_, _) -->
     [Token],
     {
         \+ Token = keyword(end) at _,
@@ -81,16 +82,16 @@ file_name_to_import(Start, _) -->
 %                                   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-define_statement(Start, define(Name, Arguments, Type, Value) at Start) -->
+define_statement(Start, define(Name, Arguments, Type, Value) at Start, Ops) -->
     valid_variable_name(Name),
     !,
     formal_parameters(Arguments),
     expected_token(Start, ':', colon, _),
     type(Type),
     expected_token(Start, operator('='), 'assignment operator', _),
-    expression_top_level(Start, Value),
+    expression_top_level(Start, Value, Ops),
     expected_token(Start, operator(end), 'end keyword', _).
-define_statement(Start, _) -->
+define_statement(Start, _, _) -->
     { throw(error('Syntax error in definition', []) at Start) }.
 
 valid_variable_name(id(Name)) -->
@@ -134,7 +135,7 @@ type_definition_params(Start, [Param | Params]) -->
     [id(Param) at _],
     !,
     type_definition_params(Start, Params).
-type_definition_params(Start, _) -->
+type_definition_params(_, _) -->
     [Token],
     !,
     { 
@@ -150,8 +151,9 @@ type_definition_params(_, []) --> [].
 type_definition_cases(Start, [Constructor | Constructors]) -->
     [keyword(case) at CaseStart],    
     !,
-    type_definition_case(CaseStart, Constructor).
-type_definition_cases(Start, _) -->
+    type_definition_case(CaseStart, Constructor),
+    type_definition_cases(Start, Constructors).
+type_definition_cases(_, _) -->
     [Token],
     !,
     {
@@ -171,7 +173,7 @@ type_definition_case(CaseStart, constructor(Name, Type)) -->
 type_definition_constructor_type(_, Type) -->
     atomic_type(Type),
     !.
-type_definition_constructor_type(Start, _) -->
+type_definition_constructor_type(_, _) -->
     [Token],
     !,
     {
@@ -190,10 +192,10 @@ type_definition_constructor_type(_, undefined) --> [].
 %                                   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-operator_definition(Start, OldOperators, NewOperators) -->
+operator_definition(_, OldOperators, NewOperators) -->
     [operator(Op) at _],
     [integer(Priority) at PStart],
-    [id(Associativity) at AStart},
+    [id(Associativity) at AStart],
     !,
     {
         valid_priority(Priority, PStart),
@@ -204,10 +206,14 @@ operator_definition(Start, OldOperators, NewOperators) -->
             Associativity, 
             OldOperators, 
             NewOperators
-        ).
+        )
     }.
 operator_definition(Start, _, _) -->
     { throw(error('Syntax error in operator definition', []) at Start) }.
+
+operator(Priority, Assoc, Op, Pos, Ops) -->
+    [operator(Op) at Pos],
+    { is_operator_type(Op, Priority, Assoc, Ops) }.
 
 valid_priority(N, _) :-
     member(N, [0,1,2,3,4,5]),
@@ -240,14 +246,14 @@ update_operators(Op, Priority, Assoc, Ops, NewOps) :-
     !,
     put_dict((Op, infix), Ops, (Priority, Assoc), NewOps).
 update_operators(Op, Priority, UnaryAssoc, Ops, NewOps) :-
-    put_dict(Op, UnaryAssoc), Ops, (Priority, UnaryAssoc), NewOps).
+    put_dict((Op, UnaryAssoc), Ops, (Priority, UnaryAssoc), NewOps).
 
 is_operator_type(Op, Priority, Assoc, Operators) :-
     member(Assoc, [left, right, none]),
     !,    
     get_dict((Op, infix), Operators, (Priority, Assoc)).
 is_operator_type(Op, Priority, UnaryAssoc, Operators) :-
-    get_dict((Op, UnaryAssoc), Operators, (Priority, Assoc)).
+    get_dict((Op, UnaryAssoc), Operators, (Priority, UnaryAssoc)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                   %
@@ -317,37 +323,37 @@ atomic_type(Type) -->
 %                                   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-expression_top_level(Start, Expr at Start) -->
-    expression_sequence(Expr),
+expression_top_level(Start, Expr at Start, Operators) -->
+    expression_sequence(Expr, Operators),
     !.
-expression_top_level(Start, _) -->
+expression_top_level(Start, _, _) -->
     { throw(error('Syntax error in expression', []) at Start) }.
 
-expression_sequence([Expr | Exprs]) -->
-    expression(Expr),
+expression_sequence([Expr | Exprs], Operators) -->
+    expression(Expr, Operators),
     !,
-    expression_sequence_tail(Exprs).
-expression_sequence_tail([Expr | Exprs]) -->
+    expression_sequence_tail(Exprs, Operators).
+expression_sequence_tail([Expr | Exprs], Operators) -->
     [';' at _],
     !,
-    expression(Expr),
-    expression_sequence_tail(Exprs).
-expression_sequence_tail([]) --> [].
+    expression(Expr, Operators),
+    expression_sequence_tail(Exprs, Operators).
+expression_sequence_tail([], _) --> [].
 
-expression(PMatch) -->
+expression(PMatch, Operators) -->
     [keyword(match) at Start],
     !,
-    pattern_matching(Start, Pmatch).
-expression(LetDef) -->
+    pattern_matching(Start, PMatch, Operators).
+expression(LetDef, Operators) -->
     [keyword(let) at Start],
     !,
-    let_definition(Start, LetDef).
-expression(Conditional) -->
+    let_definition(Start, LetDef, Operators).
+expression(Conditional, Operators) -->
     [keyword(if) at Start],
     !,
-    conditional_expression(Start, Conditional).
-expression(Tuple) -->
-    tuple_expression(Tuple).
+    conditional_expression(Start, Conditional, Operators).
+expression(Tuple, Operators) -->
+    tuple_expression(Tuple, Operators).
 % TODO: consider changing tuple priority to be
 %       lower than other expression types in 
 %       expression(...) rule
@@ -358,6 +364,42 @@ expression(Tuple) -->
 %                                   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+tuple_expression(Expr, Operators) -->
+    expr_n(0, Lhs, Operators),
+    tuple_expression_tail(Rhs, Operators),
+    { construct_tuple([Lhs | Rhs], Expr) }.
+tuple_expression_tail([Expr | Exprs], Operators) -->
+    [',' at _],
+    !,
+    expr_n(0, Expr, Operators),
+    tuple_expression_tail(Exprs, Operators).
+tuple_expression_tail([], _) --> [].
+
+expr_n(Priority, Expr, Operators) -->
+    expr_r(Priority, Lhs, Operators),
+    expr_n_rest(Priority, Rhs, Operators),
+    { merge_expr(Lhs, Rhs, Expr) }.
+expr_n_rest(Priority, [Op, Rhs], Operators) -->
+    operator(Op, Priority, none, _, Operators),
+    !,
+    expr_r(Priority, Rhs, Operators).
+expr_n_rest(_, [], _) --> [].
+
+expr_r(Priority, Expr, Operators) -->
+    expr_l(Priority, Lhs, Operators),
+    expr_l_rest(Priority, Rhs, Operators),
+    { merge_expr(Lhs, Rhs, Expr) }.
+expr_r_rest(Priority, [Op, Expr], Operators) -->
+    operator(Op, Priority, right, _, Operators),
+    expr_l(Priority, Rhs, Operators),
+    expr_r_rest(Priority, ExprRest, Operators),
+    { merge_expr(Rhs, ExprRest, Expr) }.
+
+merge_expr(Lhs, [Op, Rhs], Expr) :-
+    Expr =.. [Op, Lhs, Rhs],
+    !.
+merge_expr(Lhs, [], Lhs).
+
 
 % TODO: 1. lists, 2. unit type
 
@@ -367,18 +409,18 @@ expression(Tuple) -->
 %                                   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-conditional_expression(Start, if(Cond, Cons, Alt)) -->
-    expression_top_level(Start, Cond),
+conditional_expression(Start, if(Cond, Cons, Alt), Operators) -->
+    expression_top_level(Start, Cond, Operators),
     expected_token(Start, keyword(then), 'then keyword', ThenStart),
-    expression_top_level(ThenStart, Cons),
+    expression_top_level(ThenStart, Cons, Operators),
     conditional_expression_else(Start, Alt).
 
-conditional_expression_else(_, unit) -->
+conditional_expression_else(_, unit, _) -->
     [keyword(end) at _],
     !.
-conditional_expression_else(Start, Alt) -->
+conditional_expression_else(Start, Alt, Operators) -->
     expected_token(Start, keyword(else), 'else keyword', ElseStart),
-    expression_top_level(ElseStart, Alt),
+    expression_top_level(ElseStart, Alt, Operators),
     expected_token(Start, keyword(end), 'end keyword', _).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -387,14 +429,14 @@ conditional_expression_else(Start, Alt) -->
 %                                   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-let_definition(Start, let(Name, Type, Value, Expression)) -->
+let_definition(Start, let(Name, Type, Value, Expression), Operators) -->
     let_definition_name(Start, Name),
     expected_token(Start, ':', 'colon operator', _),
     type(Type),
     expected_token(Start, operator('='), 'assignment operator', _),
-    expression_top_level(Start, Value),
+    expression_top_level(Start, Value, Operators),
     expected_token(Start, keyword(in), 'in keyword', _),
-    expression_top_level(Start, Expression)
+    expression_top_level(Start, Expression, Operators),
     expected_token(Start, keyword(end), 'end keyword', _).
 
 let_definition_name(_, Name) -->
@@ -409,28 +451,28 @@ let_definition_name(Start, _) -->
 %                                   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-pattern_matching(Start, pmatch(Expr, Patterns)) -->
-    expression_top_level(Start, Expr),
+pattern_matching(Start, pmatch(Expr, Patterns), Operators) -->
+    expression_top_level(Start, Expr, Operators),
     expected_token(Start, keyword(with), 'with keyword', 'pattern matching'),
-    pattern_matching_cases(Start, Patterns),
+    pattern_matching_cases(Start, Patterns, Operators),
     expected_token(Start, keyword(end), 'end keyword', 'pattern matching').
 
-pattern_matching_cases(Start, [Case | Cases]) -->
-    pattern_matching_case(Start, Case),
+pattern_matching_cases(Start, [Case | Cases], Operators) -->
+    pattern_matching_case(Start, Case, Operators),
     !,
-    pattern_matching_cases(Start, Cases).
-pattern_matching_cases(_, []) --> [].
+    pattern_matching_cases(Start, Cases, Operators).
+pattern_matching_cases(_, [], _) --> [].
 
-pattern_matching_case(Start, '=>'(Pattern, Expr)) -->
+pattern_matching_case(Start, '=>'(Pattern, Expr), Operators) -->
     [keyword(case) at _],
-    pattern(Start, Pattern),
+    pattern_guard(Start, Pattern),
     expected_token(
         Start, 
         operator('=>'), 
         '=> operator', 
         'pattern matching case'
     ),
-    expression_top_level(Start, Expr).
+    expression_top_level(Start, Expr, Operators).
 
 pattern_guard(_, Pattern) -->
     pattern(Pattern),
@@ -514,7 +556,7 @@ construct_with_functor(F, [X | Xs], Result) :-
 expected_token(_, TokenVal, _, Pos) -->
     [TokenVal at Pos],
     !.
-expected_token(Start, _, TokenName _) -->
+expected_token(Start, _, TokenName, _) -->
     { throw(error('Missing expected ~w.', [TokenName]) at Start) }.
 
 throw_invalid_token(Expected, Context, Token at Pos) :-
@@ -534,5 +576,4 @@ throw_invalid_token(Expected, Context, Token at Pos) :-
         ) at Pos
     ).
 
-peek(Token), [Token] -->
-    [Token].
+peek(Token), [Token] --> [Token].
